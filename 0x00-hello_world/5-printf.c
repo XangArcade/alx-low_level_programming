5751cf1c54bd115649ad70de527d68b237898514
XangArcade <xangarcade@gmail.com>
2023-12-15 01:20:38 +0300

    Corrections commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - using printf to output the exact words
- *        followed by a new line
+ * main - the program prints "with proper grammar, but the outcome is a piece
+ *        of art" followed by a new line
  *
  * return: always 0
  */
@@ -10,5 +10,5 @@ int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art, \n");
 
-	return 0;
+	return (0);
 }
