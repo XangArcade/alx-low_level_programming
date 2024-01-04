667080e71ba0707c5b10ca90b269d1653cd2260d
XangArcade <xangarcade@gmail.com>
2024-01-04 23:55:08 +0300

    Checking for betty errors

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -20,7 +20,8 @@ char *_strcat(char *dest, char *src)
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
-		i++;															j++;														}
+		i++;
+		j++;														}
 	dest[i] = '\0';
 	return (dest);
 }
